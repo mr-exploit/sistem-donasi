8d6866ca6164f599938965296277fcb4c8074180
unknown <alfinohasan629@gmail.com>
2025-01-12 15:17:47 +0700

    Add to component footer

---
+++ resources/views/Layouts/user/footer.blade.php
@@ -0,0 +1,100 @@
+<link rel="stylesheet" href="{{asset('css/footer.css')}}">
+<!-- Footer -->
+<footer
+        class="text-center text-lg-start mt-4"
+        style="background-color: #DAFFDA; width: 100%; padding-left: 5%; padding-right: 5%;">
+  <!-- Grid container -->
+  <div class="p-4 pb-0">
+    <!-- Section: Links -->
+    <section>
+      <!--Grid row-->
+      <div class="row">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3" style="width:324px">
+          <img src="{{asset('user/Logo_NU_CARE-LAZISNU 1.svg')}}" alt="Logo Nu" class="img-fluid mb-2" >
+          <p style="color:#858E85">
+            LazisNU Kota Batam adalah Lembaga Amil Zakat, Infak, dan Sedekah Nahdatul Ulama yang berada di Kota Batam. Organisasi ini berfokus pada pemberdayaan masyarakat melalui pemanfaatan dana zakat, infak, wakaf, dan dana keagamaan lainnya untuk berbagai program sosial, seperti pendidikan, kesehatan, kemanusiaan, ekonomi, dan lingkungan.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <hr class="w-100 clearfix d-md-none" />
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h6 class="text-uppercase mt-4 font-weight-bold" style="color: #000000" id="product-text">Alamat</h6>
+          <p class="d-flex flex-row" id="location-component">
+            <img src="{{asset('user/location.svg')}}" alt="location" id="location-icon">
+            <a style="color: #858E85; text-decoration: none; margin-left: 10px" id="location-text">Perumahan Kopkar PLN, Batam Center</a>
+          </p>
+          <p class="d-flex flex-row gap-1" id="telepon-component">
+            <img src="{{asset('user/telp.svg')}}" alt="telepon" id="telepon-icon">
+            <a style="color: #858E85; text-decoration: none; margin-left: 10px" id="telepon-text">Telp : (021) <br> Hp/WA  0896202891</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <hr class="w-100 clearfix d-md-none" />
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h6 class="text-uppercase mt-4 font-weight-bold" style="color: #000000" id="selengkapnya-text">
+            Selengkapnya
+          </h6>
+          <p>
+            <a style="color: #858E85; text-decoration: none;" >Kebijakan Privasi</a>
+          </p>
+          <p>
+            <a style="color: #858E85; text-decoration: none;">Syarat dan Ketentuan</a>
+          </p>
+          <p>
+            <a style="color: #858E85; text-decoration: none;">Berita</a>
+          </p>
+          <p>
+            <a style="color: #858E85; text-decoration: none;">Report Tahunan</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <hr class="w-100 clearfix d-md-none" />
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mt-4 font-weight-bold" style="color: #000000" id="media-sosial-text">
+                Media Sosial
+            </h6>
+            
+            <div class="d-flex flex-row gap" id="media-sosial-component">
+                <img src="{{asset('user/Facebook Circled.svg')}}" alt="facbook" style="width: 20%;">
+                <img src="{{asset('user/Instagram.svg')}}" alt="instagram" style="width: 20%;">
+                <img src="{{asset('user/YouTube.svg')}}" alt="Youtube" style="width: 20%;">
+            </div>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+
+
+    <!-- Section: Copyright -->
+    <section class="p-3 pt-0">
+      <div class="row d-flex align-items-center">
+        <!-- Grid column -->
+        <div class="col-md-7 col-lg-8 text-center text-md-start">
+          <!-- Removed the copyright block here -->
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+           <a style="text-decoration: none; color: #696969">Copyright © 2024 LAZISNU Kota Batam</a>
+        </div>
+        <!-- Grid column -->
+      </div>
+    </section>
+    <!-- Section: Copyright -->
+  </div>
+  <!-- Grid container -->
+</footer>
+<!-- Footer -->
