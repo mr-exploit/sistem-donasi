b051bf2c7c191676cc2a2a2fca01849ed93aa2b7
unknown <alfinohasan629@gmail.com>
2025-01-12 17:06:09 +0700

    add to route program and berita

--- resources/views/Layouts/user/navbar.blade.php
+++ resources/views/Layouts/user/navbar.blade.php
@@ -5,10 +5,10 @@
       <!-- Left Section: Logo and Title -->
       <div class="d-flex align-items-center gap-2 " style="margin-left: 5%" id="content-nav-start">
           <img src="{{asset('user/Logo_NU_CARE-LAZISNU 1.svg')}}" alt="Logo" class="img-fluid" style="max-width: 50px; height: auto;" >
-          <div class="d-flex flex-row gap-1">
-            <h3 class="mb-0" style="font-size: 1rem; color: #007646;">LAZISNU</h3>
-            <h3 class="mb-0" style="font-size: 1rem;">KOTA BATAM</h3>
-          </div>
+            <a href="{{route('home')}}" style="text-decoration: none;" class="d-flex flex-row gap-2">
+              <h3 class="mb-0" style="font-size: 1rem; color: #007646;">LAZISNU</h3>
+              <h3 class="mb-0" style="font-size: 1rem; color:black">KOTA BATAM</h3>
+            </a>
       </div>
   
       <!-- Right Section: Navbar Toggle and Links -->
@@ -19,7 +19,7 @@
       <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup" style="margin-right: 5%">
         <div class="navbar-nav">
           <a class="nav-link active" aria-current="page" href="{{route('program')}}" >Program</a>
-          <a class="nav-link" href="#" >Berita</a>
+          <a class="nav-link" href="{{route('berita')}}" >Berita</a>
           <a class="nav-link" href="#" >Tentang Kami</a>
           <button type="button" class="btn btn-success" style="margin-left: 10px; width:80px;">Login</button>
         </div>
