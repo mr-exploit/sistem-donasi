73ddb2c0cad0520ff2aafbad30c7590eaad3453c
unknown <alfinohasan629@gmail.com>
2025-01-12 15:15:23 +0700

    add to navbar component

--- resources/views/Layouts/user/navbar.blade.php
+++ resources/views/Layouts/user/navbar.blade.php
@@ -1,25 +1,14 @@
-<style>
-   .nav-link {
-    color: #878787d1; /* Default color */
-    text-decoration: none; /* Removes underline */
-    transition: color 0.3s ease; /* Smooth transition */
-  }
-
-  .nav-link:hover {
-    color: #111111; /* Bright green for hover */
-    text-decoration: none; /* Optional underline */
-  }
-  </style>
+<link rel="stylesheet" href="{{asset('css/navbar.css')}}">
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
       <!-- Left Section: Logo and Title -->
-      <div class="d-flex align-items-center gap-2" style="margin-left: 10%">
-        <img src="{{asset('user/Logo_NU_CARE-LAZISNU 1.svg')}}" alt="Logo" class="img-fluid" style="max-width: 50px; height: auto;">
-        <div>
-          <h3 class="mb-0" style="font-size: 1rem; color: #007646;">LAZISNU</h3>
-          <h3 class="mb-0" style="font-size: 1rem;">KOTA BATAM</h3>
-        </div>
+      <div class="d-flex align-items-center gap-2 " style="margin-left: 5%" id="content-nav-start">
+          <img src="{{asset('user/Logo_NU_CARE-LAZISNU 1.svg')}}" alt="Logo" class="img-fluid" style="max-width: 50px; height: auto;" >
+          <div class="d-flex flex-row gap-1">
+            <h3 class="mb-0" style="font-size: 1rem; color: #007646;">LAZISNU</h3>
+            <h3 class="mb-0" style="font-size: 1rem;">KOTA BATAM</h3>
+          </div>
       </div>
   
       <!-- Right Section: Navbar Toggle and Links -->
@@ -27,9 +16,9 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup" style="margin-right: 15%">
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup" style="margin-right: 5%">
         <div class="navbar-nav">
-          <a class="nav-link active" aria-current="page" href="#" >Program</a>
+          <a class="nav-link active" aria-current="page" href="{{route('program')}}" >Program</a>
           <a class="nav-link" href="#" >Berita</a>
           <a class="nav-link" href="#" >Tentang Kami</a>
           <button type="button" class="btn btn-success" style="margin-left: 10px; width:80px;">Login</button>
